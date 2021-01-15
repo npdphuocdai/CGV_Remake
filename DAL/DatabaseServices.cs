4cbffba2c9dd6e34df2118e17b236f1ac7111117
npdphuocdai <68583188+npdphuocdai@users.noreply.github.com>
2021-01-15 14:26:11 +0700

    DD

--- DAL/DatabaseServices.cs
+++ DAL/DatabaseServices.cs
@@ -13,7 +13,7 @@ namespace DAL
         /// <summary>
         /// Tạo Connecting String để kết nối với Database
         /// </summary>
-        public string connectionString = @"Data Source = NPD-PHUOCDAI; Initial Catalog = CGV; Integrated Security = True;";
+        public string connectionString = @"Data Source = NPD-PhuocDai; Initial Catalog = CGV; User ID = sa; Password = dai0947170732;";
         public SqlConnection connection;
         public SqlCommand command;
         public DatabaseServices() 
